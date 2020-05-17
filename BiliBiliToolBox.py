287f87cf24532d57f417398128d5a55d9778ab23
Yang KeMing <54489833+yangkeming@users.noreply.github.com>
2020-05-17 18:14:38 +0800

    Update BiliBiliToolBox.py

--- BiliBiliToolBox.py
+++ BiliBiliToolBox.py
@@ -8,6 +8,7 @@ import sys
 import os.path
 import json
 import stat
+from you_get import common as you_get 
 def geturl(url):
     webpage = req.urlopen(url)
     data = webpage.read()
@@ -24,11 +25,11 @@ else:
 curpath=os.path.dirname(os.path.realpath(__file__))
 conf=configparser.RawConfigParser()
 conf.read("config.ini")
-if info == 1:    
-    api = conf.get("api","bvtoav").replace('%bv%',shuju).replace('%nobvbv%',shuju[2:])
-else:
-    api = conf.get("api","avtobv").replace('%av%',shuju).replace('%noavaid%',shuju[2:])
 ver = conf.get("ver","version")
+if info == 1:    
+    api = conf.get("api","bvtoav").replace('%bv%',shuju).replace('%nobvbv%',shuju[2:]).replace('%ver',ver)
+else:
+    api = conf.get("api","avtobv").replace('%av%',shuju).replace('%noavaid%',shuju[2:]).replace('%ver%',ver)
 updata = conf.get("updata","server").replace('%ver%',ver)
 if geturl(updata)!='SUCCESS':
     easygui.msgbox(geturl(updata))
@@ -61,5 +62,15 @@ print(str(json['data']['stat']['favorite'])+'个收藏')
 print(str(json['data']['stat']['coin'])+'个硬币')
 print(str(json['data']['stat']['share'])+'次分享')
 print(str(json['data']['stat']['like'])+'个点赞')
+lll = int(input("\n输入1下载此视频，输入2退出："))
+if lll==2:
+    exit()
+elif lll==1:
+        directory = r'./'                         #设置下载目录
+        url = 'https://www.bilibili.com/video/'+shuju      #需要下载的视频地址
+        sys.argv = ['you-get','-o',directory,url]       #sys传递参数执行下载，就像在命令行一样
+        you_get.main()
+else:
+    print("ERROR!!!")
+    exit()
 print('\n\n\nBILIBILI TOOL 2020 By YKM\n\n\n')
-os.system('pause')
